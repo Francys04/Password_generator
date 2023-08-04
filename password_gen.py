e67912e5abd4617609825443139cd421143678ea
Fransys1234 <ciocarlangalina1971@gmail.com>
2023-08-04 15:55:15 +0300

    commit4

--- password_gen.py
+++ password_gen.py
@@ -1,27 +1,33 @@
+"""Generating random characters"""
 import random
-# for all characters 
+"""For all characters""" 
 import string
 
-
+"""store the sets of ASCII letters, digits (0-9), and special characters, respectively."""
 def generate_password(min_lenght, numbers=True, special_characters=True):
-    letters = string.ascii_letters
-    digits = string.digits
-    special = string.punctuation
+    letters = string.ascii_letters #The minimum length of the password to be generated.
+    digits = string.digits #A boolean flag to indicate whether numbers should be included in the generated password (default is True).
+    special = string.punctuation #A boolean flag to indicate whether special characters should be included in 
+    #the generated password (default is True).
 
-    # print(letters, special, digits)
-    # combine in list
+    """print(letters, special, digits) and combine in list"""
+    
     characters = letters
     if numbers:
         characters += digits
     if special_characters:
         characters += special
 
-    pwd = ""
-    # include just number or special char or all of this
+    pwd = "" #store the generated password
+    
+    """Include just number or special char or all of this"""
+    """track whether the generated password meets the specified criteria."""
     meets_criteria = False
     has_number = False
     has_special = False
-
+    
+    """This while loop generates the password character by character using the 
+    random.choice() function to select a random character from the characters set and adds it to the pwd string."""
     while not meets_criteria or len(pwd) < min_lenght:
         new_char = random.choice(characters)
         pwd += new_char
@@ -40,8 +46,7 @@ def generate_password(min_lenght, numbers=True, special_characters=True):
     return pwd
 
 
-# call the func
-# generate_password(10, True, False)
+"""Call the func and generate_password(10, True, False)"""
 
 min_lenght = int(input("Enter the minimum lenght: "))
 has_number = input("Do you want to have numbers (y/n)?: ").lower() == "y"
